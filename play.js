dd9bd777cdba9d8109243afa63fdb9d27b77b338
narek.pedanyan <narek.pedanyan@betconstruct.com>
2019-03-07 15:36:55 +0400

    Promises Lesson 18

--- play.js
+++ play.js
@@ -1,2 +1,22 @@
-var name = 'Narek';
-console.log(name);
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+    return promise;
+};
+
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    }).then(text2 => {
+        console.log(text2 + 'Asd');
+    });
+}, 2000);
+
+
+console.log('MEk')
+console.log('Erku')
