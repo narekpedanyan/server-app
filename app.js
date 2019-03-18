49e0b5b02de030d776a1e2abfd04dd9258c3deef
narek.pedanyan <narek.pedanyan@betconstruct.com>
2019-03-18 13:24:22 +0400

    Create server template.

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ function rqListener(req, res) {
 }
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>my first page</title></head>');
+    res.write('<body>');
+    res.write('<p>asdf</p>')
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3002);
