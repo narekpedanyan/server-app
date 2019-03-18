5e7545d5eb920353a1ffa79d8e2ff91769516771
narek.pedanyan <narek.pedanyan@betconstruct.com>
2019-03-18 13:38:58 +0400

    Req url

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ function rqListener(req, res) {
 }
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>my first page</title></head>');
